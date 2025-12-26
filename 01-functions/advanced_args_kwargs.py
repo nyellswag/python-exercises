da1b9b15b4af31493a667d308b80b740e47bb03e
nyellswag <105097535+nyellswag@users.noreply.github.com>
2025-12-26 22:20:59 +0400

    Update advanced_args_kwargs.py

--- 01-functions/advanced_args_kwargs.py
+++ 01-functions/advanced_args_kwargs.py
@@ -20,3 +20,24 @@ def bulid_message(greeting, name, **kwargs):
     for key, value in kwargs.items():
         print(f"{key}:{value}")
 bulid_message("Привет", "Мария", age=25, city="СПб", hobby="рисование")
+
+
+# Add person function with kwargs
+
+def add_person(people_list, name, age):
+    people_list.append((name, age))
+
+
+def add_people(people_list, **people):
+    for name, age in people.items():
+        people_list.append((name, age))
+
+
+data = [("Аня", 22), ("Борис", 31), ("Вика", 28)]
+
+add_person(data, "Дима", 25)
+
+extra = {"Коля": 19, "Марина": 30}
+add_people(data, **extra)
+
+print(data)
